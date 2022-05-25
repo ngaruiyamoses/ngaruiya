d030f4a07cc8a94dea8ec02542b946ef69976c5b
ngaruiya <mosengaruiya704@gmail.com>
2022-05-25 09:36:47 +0300

    assignment5.py

---
+++ assignment5.py
@@ -0,0 +1,16 @@
+#####
+vehicles = ["bmw","audi","cadillac","mercedes","jeep"]
+
+
+#using a for loop to print all vehicles
+for vehicle in vehicles:
+    print(vehicle.title())
+    if(vehicle == "jeep"):
+        print(vehicle.upper())
+    if(vehicle == "cadillac"):
+        print(vehicle.upper())
+    if(vehicle == [4]):
+        print(vehicle.upper())
+
+
+#####
