2c059fe39e98ffea830500c143656d057d9ce4c4
ngaruiya <mosengaruiya704@gmail.com>
2022-07-06 10:53:04 +0300

    index.html

---
+++ web/js/index.js
@@ -0,0 +1,16 @@
+alert("javascript linked successfully")
+console.log("rada rada")
+let message = "awooooo"
+console.log(message)
+//single line comment in javascript
+/*
+multi line comment in javascript
+*/
+let num1 = 134
+let num2 = 134
+let num = num1 + num2
+console.log(num)
+
+function submit(){
+    alert("button clicked")
+}    
